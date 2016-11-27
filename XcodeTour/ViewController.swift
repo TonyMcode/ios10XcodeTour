f24ffd7e66ed0e2f71a91728a8a2257f9825a4e0
tmega <tony@Tonys-MacBook-Pro-1220.local>
2016-11-27 12:23:20 -0800

    initial commit

---
+++ XcodeTour/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  XcodeTour
+//
+//  Created by Tony on 11/26/16.
+//  Copyright © 2016 Tony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var colorView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func blueBtnTapped(_ sender: Any) {
+        colorView.backgroundColor = UIColor.blue
+    }
+    
+    @IBAction func greenBtnTapped(_ sender: Any) {
+        colorView.backgroundColor = UIColor.green
+    }
+
+}
+
